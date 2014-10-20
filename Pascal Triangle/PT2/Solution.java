0ff5f654fc8292c3befe1ab46fcb7fe77a4d9048
陈曦 <clarkchen@foxmail.com>
2014-10-20 21:13:13 +0800

    pascals-triangle-ii 这道题是一道数列推导的题目，需要熟悉公式 例如 1*2 + 2*3 +。。。。+ n*(n+1)  = n(n+1)(n+2)/3

---
+++ Pascal Triangle/PT2/Solution.java
@@ -0,0 +1,27 @@
+package PT2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+ public class Solution {
+	public List<Long> getRow(int numRows) {
+		if(numRows<0  ) return  new ArrayList<Long>();
+		List<Long> rt = new ArrayList<Long>();
+		rt.add((long)1);
+		int row = numRows+1;
+		for(int i=1;i<row;i++)
+		{
+			rt.add(rt.get(i-1)*(row-i)/i);
+		}
+		
+		return rt;
+	}
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s= new Solution();
+		int a=5;
+		System.out.println(s.getRow(3));
+	}
+
+}
