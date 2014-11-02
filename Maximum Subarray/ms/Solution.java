f1b64404ac51aba17f8c4879a00198beb9fc4fb7
陈曦 <clarkchen@foxmail.com>
2014-11-02 20:48:23 +0800

    最大和，动态规划

---
+++ Maximum Subarray/ms/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ms;
+import java.util.*;
+/**
+ * @author chenxi
+ *
+ */
+public class Solution {
+	public int maxSubArray(int[] A) {
+		if(A==null ) return 0;
+        int sum [] = new int [A.length];
+        sum[0] = A[0];
+        int max = sum[0];
+        for(int i=1;i<A.length;i++)
+        {
+        	sum[i] = sum[i-1]>0?sum[i-1]+A[i]:A[i];
+        	max = sum[i]>max?sum[i]:max;
+        }
+        
+        return max;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s= new Solution();
+		int []A= {-1};
+		System.out.println(s.maxSubArray(A));
+	}
+
+}
