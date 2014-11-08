0c467305ccfe20d0260ae1b67243aa7e335cd886
陈曦 <clarkchen@foxmail.com>
2014-11-08 20:16:59 +0800

    排序的补充

--- Utils/Sort/listsort.java
+++ Utils/Sort/listsort.java
@@ -17,12 +17,23 @@ public class listsort {
 		}
 		
 	}
-	public class CustomComparator implements Comparator<NodeValue> {
+	//升序排列
+	public class IncreaseComparator implements Comparator<NodeValue> {
 	    @Override
 	    public int compare(NodeValue o1, NodeValue o2) {
 	        return o1.v-o2.v;
 	    }
 	}
+	
+	//降序排列
+	public class DecreaseComparator implements Comparator<NodeValue> {
+	    @Override
+	    public int compare(NodeValue o1, NodeValue o2) {
+	        return o2.v-o1.v;
+	    }
+	}
+	
+	 
 	public void test()
 	{
 		ArrayList<listsort.NodeValue> al = new ArrayList<listsort.NodeValue>();
@@ -30,13 +41,21 @@ public class listsort {
 		listsort.NodeValue n2=  new listsort.NodeValue(1, 6);
 		NodeValue n3 = new NodeValue(4,5);
 		al.add(n1);al.add(n2);al.add(n3);
+		System.out.println("原序列");
 		for(NodeValue s:al)
 			System.out.println(s.v+" "+s.index);
 		
-		Collections.sort(al, new CustomComparator());
+		System.out.println("上升序列");
+		//increasing order
+		Collections.sort(al, new IncreaseComparator());
 		for(NodeValue s:al)
 			System.out.println(s.v+" "+s.index);
 		
+		System.out.println("下降序列");
+		//decreasing order
+		Collections.sort(al, new DecreaseComparator());
+		for(NodeValue s:al)
+			System.out.println(s.v+" "+s.index);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
