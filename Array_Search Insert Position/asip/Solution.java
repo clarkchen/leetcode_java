2163975a32246a1d6b7e38f70c348d7bc596aa44
chenxi <chenxi@wecash.net>
2015-02-26 10:30:22 +0800

    修改为单元测试的模式

--- Array_Search Insert Position/asip/Solution.java
+++ Array_Search Insert Position/asip/Solution.java
@@ -3,12 +3,14 @@ package asip;
 public class Solution {
 	 
     public int searchInsert(int[] A, int target) {
-       int end = A.length;
-       if (end==0) return 0;
+    	
+       //边界条件判断
+       if ( A.length==0 ) return 0;
+       int end = A.length-1;
        int start = 0 ;
        if (A[start] >= target) return start;
-       else if (A[end-1]< target ) return end;
-       end = end-1;
+       else if (A[end]< target ) return A.length;
+       //二分查找
        int mid = (end-start)/2;
        while ( end>=start)
        {
@@ -24,19 +26,9 @@ public class Solution {
     	   }
     	   mid = (end+start)/2;
        }
-       if (A[end]>target) return end;
-       
        return end+1;
        
     
     }
- 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Solution s = new Solution();
-		int []a = new int[] {1,3};
-		int target = 3;
-		System.out.println(s.searchInsert(a, target)); 
-	}
 
 }
