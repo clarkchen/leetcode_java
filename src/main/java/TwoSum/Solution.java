d6f15ccd88ebc46b77816dc3455587fb8e0d54b5
clarkchen <clarkchen@foxmail.com>
2015-04-28 15:42:45 +0800

    二分查找的方式 nlogn 的复杂度依然超时

--- src/main/java/TwoSum/Solution.java
+++ src/main/java/TwoSum/Solution.java
@@ -8,6 +8,7 @@ import java.util.List;
  * Created by chenxi on 15/4/30.
  */
 public class Solution {
+    //暴力搜搜
     public int[] bruteFroce(int []nums, int target)
     {
         int i=0,j=0;
@@ -25,7 +26,8 @@ public class Solution {
         }
         return  new int[]{};
     }
-    public int[] sort(int []nums, int target)
+    //排序+二分查找的方法
+    public int[] sortType(int []nums, int target)
     {
         int [] reverse = new int[nums.length];
         int i=0, j=0;
@@ -35,16 +37,42 @@ public class Solution {
         }
 
         Arrays.sort(reverse);
+        int one=Integer.MIN_VALUE , theOther=Integer.MIN_VALUE;
         for (i=0;i<nums.length;i++)
         {
-            
+            int find = Arrays.binarySearch(reverse, nums[i]);
+            if (find <0) continue;
+            one = reverse[find];
+            if (target%2==0 && one ==  target/2)
+            {
+               if(find+1 < nums.length && reverse[find+1]==one){}
+               else if(find-1>=0 && reverse[find-1] ==one){}
+               else continue;
+            }
+            theOther = target - one;
+            break;
         }
 
-        return  new int[]{};
+        if( theOther==Integer.MIN_VALUE) return  new int[]{};
+
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==one) {one=i+1;break;}
+        }
+        nums[i] = Integer.MIN_VALUE;
+
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==theOther) {theOther=i+1;break;}
+        }
+        i = one>theOther? theOther:one;
+        j = one>theOther? one: theOther;
+        return  new int[]{i,j};
 
     }
     public int[] twoSum(int[] nums, int target) {
-       return bruteFroce(nums, target);
+
+        return sortType(nums, target);
     }
 
 
