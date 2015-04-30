c110913ad194e785cfc026cd48a5ca13f8b3e373
clarkchen <clarkchen@foxmail.com>
2015-04-30 16:22:01 +0800

    通过HashTable 的方式来解决这个问题

--- src/main/java/TwoSum/Solution.java
+++ src/main/java/TwoSum/Solution.java
@@ -1,8 +1,8 @@
 package TwoSum;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.*;
 
 /**
  * Created by chenxi on 15/4/30.
@@ -26,7 +26,9 @@ public class Solution {
         }
         return  new int[]{};
     }
-    //排序+二分查找的方法
+
+
+    //排序+二分查找的方法 n logn 的复杂度
     public int[] sortType(int []nums, int target)
     {
         int [] reverse = new int[nums.length];
@@ -70,9 +72,46 @@ public class Solution {
         return  new int[]{i,j};
 
     }
+    //集合的方法 2n + logn 的复杂度
+    public int[] setType(int []nums, int target)
+    {
+//        int [] reverse = new int[nums.length];
+        int i=0, j=0;
+        HashMap<Integer, Integer> hm =  new HashMap<Integer, Integer>();
+        for (i =0 ;i< nums.length; i++)
+        {
+            int reverse = target -  nums[i];
+            hm.put(reverse, hm.getOrDefault(reverse,0)+1);
+        }
+
+        for (i =0 ;i< nums.length; i++)
+        {
+            int one =  nums[i];
+            if (!hm.containsKey(one)) continue;
+            if (target%2==0 && one ==  target/2)
+            {
+                if (hm.get(one)==1) continue;
+            }
+            target = target - one;
+            break;
+        }
+        if (i==nums.length)
+        {
+            return  new int[]{};
+        }
+        for (j=0;j<nums.length;j++)
+        {
+            if(i==j) continue;
+            if (target==nums[j]) break;
+        }
+        int one = i<j? i:j;
+        int two = i>j? i:j;
+        return  new int[]{one+1,two+1};
+
+    }
     public int[] twoSum(int[] nums, int target) {
 
-        return sortType(nums, target);
+        return setType(nums, target);
     }
 
 
